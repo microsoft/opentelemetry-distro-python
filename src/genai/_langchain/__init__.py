e45078e6a34ae5147f3e29a0912d2cd327fa5092
Radhika Gupta <guptaradhika@microsoft.com>
2026-04-09 09:30:29 -0700

    Add langchain instrumentation

---
+++ src/genai/_langchain/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+from genai._langchain.tracer_instrumentor import LangChainInstrumentor
+
+__all__ = ["LangChainInstrumentor"]
