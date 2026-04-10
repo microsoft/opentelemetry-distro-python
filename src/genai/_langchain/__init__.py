628020dc331a281ce18d6bb2bff70d2a93de010f
Radhika Gupta <guptaradhika@microsoft.com>
2026-04-10 09:57:49 -0700

    Move genai folder under microsoft

--- src/genai/_langchain/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
-from genai._langchain._tracer_instrumentor import LangChainInstrumentor
-
-__all__ = ["LangChainInstrumentor"]
