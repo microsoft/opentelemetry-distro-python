6905391220c0e3c7e7135a005eff006d1ecd7824
Radhika Gupta <guptaradhika@microsoft.com>
2026-06-02 08:55:25 -0700

    Add disable comment

--- samples/distro/fastapi_app.py
+++ samples/distro/fastapi_app.py
@@ -17,7 +17,7 @@ use_microsoft_opentelemetry(
 logger = getLogger(__name__)
 logger.setLevel(INFO)
 
-from fastapi import FastAPI  # pylint: disable=wrong-import-position
+from fastapi import FastAPI  # noqa: E402
 
 app = FastAPI()
 
